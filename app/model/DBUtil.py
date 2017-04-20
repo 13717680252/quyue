63d8f75b7e6bcb0121c0e27b151f5aa5befdcabe
KarlMical <KarlMical@outlook.com>
2017-04-20 12:20:23 +0800

    complete all insert functons add friends retreive and update funciton

--- app/model/DBUtil.py
+++ app/model/DBUtil.py
@@ -7,7 +7,7 @@ Created on 2017年4月14日
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import insert
-from models import TUser, TGroup, TActivity
+from models import TUser, TGroup, TActivity, TCommentActivity, TCommnetPerson
 from sqlalchemy.sql.schema import Table
 from sqlalchemy.sql.expression import exists, select
 from sqlalchemy.sql.elements import or_, literal
@@ -35,14 +35,16 @@ class DBUtil:
         rs = handle_func(ss, args)
         ss.close()
         return rs
-    
+
+
     @staticmethod
     def exec_query_with_conn(handle_func, **args):
         conn = engine.connect()
         rs = handle_func(conn, args)
         conn.close()
         return rs
-    
+
+
     @staticmethod
     def __util_check_user_name_duplicated(ss, args):
         try:
@@ -52,16 +54,18 @@ class DBUtil:
             return False, e
         
         return (rs == 1), None
-    
+
+
     @staticmethod
-    def __util_check_user_phone_dupliacted(ss, args):
+    def __util_check_user_phone_duplicated(ss, args):
         try:
             rs = ss.query(TUser.id).filter(TUser.phone == args['phonenumber']).limit(1).count()
         except Exception as e:
             print(e)
             return False, e
         return (rs == 1), None
-    
+
+
     @staticmethod
     def __util_check_user_mail_duplicate(ss, args):
         try:
@@ -70,7 +74,18 @@ class DBUtil:
             print(e)
             return False, e
         return (rs == 1), None
-    
+
+
+    @staticmethod
+    def __util_retrieve_user_friends(ss, args):
+        try:
+            rs = ss.query(TUser.friends).filter(TUser.id == args['user_id']).first()
+        except Exception as e:
+            print(e)
+            return None, e
+
+        return rs[0], None
+
     @staticmethod
     def __util_insert_new_user(conn, args):
         tb = TUser.__table__
@@ -93,9 +108,9 @@ class DBUtil:
         except Exception as e:
             print(e)
             return 0, e
-        
-        
+
         return rs.lastrowid, None
+
 
     @staticmethod
     def __util_insert_new_group(ss, args):
@@ -111,9 +126,10 @@ class DBUtil:
 
         return new_group.id, None
 
+
     @staticmethod
     def __util_insert_new_activity(ss, args):
-        print('insert a new activity')
+        #print('insert a new activity')
         new_activity = TActivity(name=args['name'], publisher=args['publisher'], group_id=args['group_id'],
                                  description=args['description'], start_date=args['start_date'], end_date=args['end_date'],
                                  min_num=args['min_num'], max_num=args['max_num'], cur_num=args['cur_num'], join_ids=args['join_ids'],
@@ -127,6 +143,60 @@ class DBUtil:
             return 0, e
 
         return new_activity.id, None
+
+
+    @staticmethod
+    def __util_insert_activity_comment(ss, args):
+        #print()
+        comm_act = TCommentActivity(activity_id=args['act_id'], comment_user_id=args['user_id'], level=args['level'],
+                                    content=args['content'])
+        try:
+            ss.add(comm_act)
+            ss.commit()
+        except Exception as e:
+            print(e)
+            ss.rollback()
+            return 0, e
+
+        return comm_act.id, None
+
+
+    @staticmethod
+    def __util_insert_comment_person(ss, args):
+        #print()
+        #TCommnetPerson.commented_user_id
+        commp = TCommnetPerson(activity_id=args['act_id'], comment_user_id=args['comm_user_id'], commented_user_id=args['commed_user_id'],
+                               level=args['level'], content=args['content'])
+        try:
+            ss.add(commp)
+            ss.commit()
+        except Exception as e:
+            print(e)
+            ss.rollback()
+            return 0, e
+
+        return commp.id, None
+
+
+    @staticmethod
+    def __util_add_user_frends(ss, args):
+        new_friends = args['new_friends']
+        old_friends, exp = DBUtil.__util_retrieve_user_friends(ss, {"user_id": args['user_id']})
+        if old_friends is None:
+            return False, None
+
+        update_freindslist = ','.join(new_friends)
+        if old_friends:
+           update_freindslist = old_friends + ',' + update_freindslist
+        try:
+            ss.query(TUser).filter(TUser.id == args['user_id']).update({TUser.friends : update_freindslist}, synchronize_session=False)
+            ss.commit()
+        except Exception as e:
+            print(e)
+            ss.rollback()
+            return False, e
+
+        return True, None
 
 
     #param 'name': the name of user
@@ -144,7 +214,7 @@ class DBUtil:
     #'error' is the exception when executing the query in the database, None means no exception
     @staticmethod
     def check_user_phone_duplicated(phone):
-        return DBUtil.exec_query(DBUtil.__util_check_user_phone_dupliacted, phonenumber=phone)
+        return DBUtil.exec_query(DBUtil.__util_check_user_phone_duplicated, phonenumber=phone)
     
     
     #param 'mail': the mail address of user
@@ -154,7 +224,17 @@ class DBUtil:
     @staticmethod
     def check_user_mail_duplicated(mail):
         return DBUtil.exec_query(DBUtil.__util_check_user_mail_duplicate, mail=mail)
-    
+
+
+    #param 'user_id': the id of query user
+    #return a tuple <friendslist, error>
+    #'friendslist': if successfully retrieved, friendslist is a str; else None
+    #'error':  the exception when executing the query in the database, None means no exception
+    @staticmethod
+    def retrieve_user_friendslist(user_id):
+        return DBUtil.exec_query(DBUtil.__util_retrieve_user_friends, user_id=user_id)
+
+
     #param 'args': a dict of user propertities
     #reutrn a tuple <rowid, error>
     #'rowid' is the last user row id, if not exists, rowid is 0
@@ -167,6 +247,7 @@ class DBUtil:
                           profession=args['profession'], sex=args['sex'], 
                           birthdate=args['birthdate'], avatar=args.get('avatar'), is_activated=args.get('is_activated'))
 
+
     #param 'args': a dict of group propertities
     #return a tuple <groupid, error>
     #'groupid' is group id of new group inserted. If failed, group id is 0
@@ -175,7 +256,8 @@ class DBUtil:
     def insert_new_group(args):
         return DBUtil.exec_query(DBUtil.__util_insert_new_group, name=args['name'], type=args.get('type'),
                                  description=args['description'], create_date=args['create_date'])
- 
+
+
     #param 'args': a dict of group properties
     # return a tuple <activity_id, error>
     # 'groupid' is id of new activity inserted. If failed, group id is 0
@@ -187,3 +269,32 @@ class DBUtil:
                                  description=args['description'], start_date=args['start_date'], end_date=args['end_date'],
                                  min_num=args['min_num'], max_num=args['max_num'], is_canceled=args['is_canceled'],cur_num=args.get('cur_num'),
                                  join_ids=args.get('join_ids'), tags=args.get('tags'))
+
+    #param 'args': a dict of commentOfActivity properties
+    # return a tuple <commnet_id, error>
+    # 'comment_id' is id of new comment inserted. If failed, commnet_id is 0
+    # 'error' is the exception when executing the query in the database, None means no exception
+    @staticmethod
+    def insert_new_comment_activity(args):
+        return DBUtil.exec_query(DBUtil.__util_insert_activity_comment,
+                                 act_id=args['act_id'], user_id=args['user_id'], level=args['level'], content=args['content'])
+
+    # param 'args': a dict of commentOfPerson properties
+    # return a tuple <comment_id, error>
+    # 'comment_id' is id of new comment inserted. If failed, commnet_id is 0
+    # 'error' is the exception when executing the query in the database, None means no exception
+    @staticmethod
+    def insert_new_comment_person(args):
+        return DBUtil.exec_query(DBUtil.__util_insert_comment_person,
+                                 act_id=args['act_id'],comm_user_id=args['comm_user_id'], commed_user_id=args['commed_user_id'],
+                                 level=args['level'], content=args['content'])
+
+
+    #param 'user_id'
+    #param 'new_friends': a str list of user id
+    #return a tuple <ok, error>
+    #'ok': a boolean value indicates whether th update is successful.
+    #'error' is the exception when executing the query in the database, None means no exception
+    @staticmethod
+    def update_user_friends(user_id, new_friends):
+        return DBUtil.exec_query(DBUtil.__util_add_user_frends, user_id=user_id, new_friends=new_friends)
